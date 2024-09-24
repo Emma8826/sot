91f9b1592b91823f4eecbdd206b4867abe41614c
Emma8826 <50663707+Emma8826@users.noreply.github.com>
2024-09-24 15:54:47 +0800

    Add files via upload

---
+++ sot/sot/src/test/java/soldoutToday/com/sot/SotApplicationTests.java
@@ -0,0 +1,13 @@
+package soldoutToday.com.sot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
